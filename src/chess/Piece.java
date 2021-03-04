543ca6290b04ddf570c81f38afd890951b55f1c0
Jahnavi Bavuluri <jbavuluri@gmail.com>
2021-03-04 02:35:44 -0500

    started initializing the Piece.java class

--- src/chess/Piece.java
+++ src/chess/Piece.java
@@ -1,1 +1,14 @@
-//comment
+//creates the abstract Piece class that the other pieces will extend
+public class Piece {
+	import java.awt.Point;
+	
+	public String color;
+	public Point location;
+	
+	//initializes the piece with a color (black or white) and a point location
+	public Piece (String color, int x, int y) {
+		this.color = color;
+		this.location = new Point(x,y);
+	}
+	 
+}
